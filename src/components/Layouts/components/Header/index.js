916dcf683d079d6cbb21efb15cf09e545180d3fe
Lam <volam13062001@gmail.com>
2023-01-21 22:02:33 +0700

    Xây Dựng UI Phần Header Dự Án Tiktok #4

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -3,8 +3,11 @@ import { useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCircleXmark,
+  faEllipsisVertical,
+  faKeyboard,
+  faLanguage,
   faMagnifyingGlass,
-  faSignIn,
+  faQuestionCircle,
   faSpinner,
 } from "@fortawesome/free-solid-svg-icons";
 import Tippy from "@tippyjs/react/headless";
@@ -12,11 +15,28 @@ import Tippy from "@tippyjs/react/headless";
 import { Wrapper as PopperWrapper } from "~/components/Popper";
 import { images } from "~/assets/images";
 import Button from "~/components/Button";
+import AccountItem from "~/components/AccountItem";
 import classNames from "classnames/bind";
 import styles from "./Header.module.scss";
-import AccountItem from "~/components/AccountItem";
+import Menu from "~/components/Popper/Menu";
 
 const cx = classNames.bind(styles);
+
+const MENU_ITEMS = [
+  {
+    icon: <FontAwesomeIcon icon={faLanguage} />,
+    title: "English",
+  },
+  {
+    icon: <FontAwesomeIcon icon={faQuestionCircle} />,
+    title: "Feedback and help",
+    to: '/feedback'
+  },
+  {
+    icon: <FontAwesomeIcon icon={faKeyboard} />,
+    title: "Keyboard shorcuts",
+  },
+];
 
 function Header() {
   const [searchResult, setSearchResult] = useState([]);
@@ -67,6 +87,12 @@ function Header() {
         <div className={cx("actions")}>
           <Button text>Upload</Button>
           <Button primary>Log in</Button>
+
+          <Menu items={MENU_ITEMS}>
+            <button className={cx("more-btn")}>
+              <FontAwesomeIcon icon={faEllipsisVertical} />
+            </button>
+          </Menu>
         </div>
       </div>
     </header>
