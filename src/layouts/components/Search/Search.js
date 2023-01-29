3eb834e5babb4b78ef9fd31fabbfbf41467ff3b6
Lam <volam13062001@gmail.com>
2023-01-30 00:00:54 +0700

    Tối ưu Header phần cuối

--- src/layouts/components/Search/Search.js
+++ src/layouts/components/Search/Search.js
@@ -16,29 +16,29 @@ const cx = classNames.bind(styles);
 function Search() {
   const [searchValue, setSearchValue] = useState("");
   const [searchResult, setSearchResult] = useState([]);
-  const [showResult, setShowResult] = useState(true);
+  const [showResult, setShowResult] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const debounced = useDebounce(searchValue, 500);
+  const debouncedValue = useDebounce(searchValue, 500);
 
   const inputRef = useRef();
 
   useEffect(() => {
-    if (!debounced.trim()) {
+    if (!debouncedValue.trim()) {
       setSearchResult([]);
       return;
     }
 
     const fetchApi = async () => {
       setLoading(true);
-      const result = await searchServices.search(debounced, 5);
+      const result = await searchServices.search(debouncedValue, 5);
 
       setSearchResult(result);
       setLoading(false);
     };
 
     fetchApi();
-  }, [debounced]);
+  }, [debouncedValue]);
 
   const handleClear = () => {
     setSearchValue("");
